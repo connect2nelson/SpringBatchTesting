6f5549868756b8f54e8eb0eb55b394642bc122a5
nelsondavenapalli <nelson.csits@gmail.com>
2018-02-01 22:08:13 +0100

    Initial Commit - Bare bones of the project

---
+++ src/main/java/fun/abm/springbatchtesting/SpringBatchTesting/SpringBatchTestingApplication.java
@@ -0,0 +1,12 @@
+package fun.abm.springbatchtesting.SpringBatchTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTestingApplication.class, args);
+	}
+}
---
+++ src/test/java/fun/abm/springbatchtesting/SpringBatchTesting/SpringBatchTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package fun.abm.springbatchtesting.SpringBatchTesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBatchTestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
